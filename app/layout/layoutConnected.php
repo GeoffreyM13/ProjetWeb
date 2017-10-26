5a8e5794af2065f2fdb1a23146b3ed08b65ed8dc
Seymour84 <dimidu13190@hotmail.fr>
2017-10-26 23:58:43 +0200

    update et mise en place du tableau de rangement utilisateur

--- app/layout/layoutConnected.php
+++ app/layout/layoutConnected.php
@@ -17,9 +17,16 @@
     <link href="css/stylebis.css" rel="stylesheet" type="text/css" >
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
+    <script type="text/javascript" language="javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" language="javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+    <script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-1.12.4.js">
+    </script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js">
+    </script>
     
-
 </head>
 <body>
 
@@ -51,13 +58,6 @@
 </nav>
 
 <!-- NAVBAR -->
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"></script>
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-
 <div id="layout"> <?php echo $context->data; ?> <?php echo $context->error; ?> </div>
 
 <?php include($template_view); ?>
@@ -73,7 +73,6 @@
                 url: '../ProjetWeb/BlackManbaAjax.php?action=disconnected',
                 success: function(data) {
                     $('#layout').empty().prepend("Vous êtes bien deconnecté!");
-                    $('#divconnected').load('app/view/loginSuccess.php .container');
                 },
                 error: function() {
                     alert('La requête n\'a pas abouti'); }
--- app/view/userslistSuccess.php
+++ app/view/userslistSuccess.php
@@ -1,50 +1,44 @@
-<?php
-/*
- * Created by PhpStorm.
- * User: geoffrey
- * Date: 20/10/2017
- * Time: 11:51
- */
-
-?>
-
-
-
-
-<div class="list_users">
-
-    <div class="col-md-6 col-md-offset-2">
-        <h2>Liste des Utilisateurs de l'application</h2>
-        <p>Vous pouvez cliquer sur un utilisateur pour voir sont profil :</p>
-        <table id="list_users" class="display" width="100%" cellspacing="0">
-            <thead>
+<!--Fait par Dimitri Hueber-->
+<style type="text/css">
+    .image-circle {
+        float: none;
+        margin: 0 auto;
+        width: 8%;
+        height: 8%;
+        -webkit-border-radius: 50% !important;
+        -moz-border-radius: 50% !important;
+        border-radius: 50% !important;
+    }
+</style>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#list_users').DataTable();
+        } );
+</script>
+    <!--Affiche un tableau avec les utilisateurs
+        le tableau peut être trier 
+        une fonction de recherche y est intégré aussi avec le js-->
+    <table id="list_users" class="display" width="100%" cellspacing="0">
+        <thead>
             <tr>
-                <th>avatar</th>
-                <th>nom</th>
-                <th>prenom</th>
-                <th>identifiant</th>
+                <th>Nom</th>
+                <th>Prenom</th>
+                <th>Identifiant</th>
+                <th>Avatar</th>
             </tr>
-            </thead>
-
-
-            <?php foreach($context->users as $data ): ?>
-
-                <tbody>
+        </thead>
+        <tbody>
+            <?php foreach($context->users as $data ){ ?>
+            <?php 
+            if ($data->nom!=context::getSessionAttribute('nom') && $data->prenom!=context::getSessionAttribute('prenom') && $data->identifiant!=context::getSessionAttribute('identifiant')){
+                ?>
                 <tr>
-
-                    <td><img class="image-circle" src="<?php echo $data->avatar; ?>" width="50" height="50"></td>
                     <td><?php echo $data->nom ?></td>
                     <td><?php echo $data->prenom ?></td>
                     <td><?php echo $data->identifiant ?></td>
-                    <td><a type="button" class="btn btn-primary" href="petitTweet.php?action=view_profilUsers&id=<?php echo $data->id ?>">Profil</a></td>
+                    <td><a type="button" href="petitTweet.php?action=view_profilUsers&id=<?php echo $data->id ?>"><img class="image-circle" src="<?php echo (!empty($context->user->avatar)?$context->user->avatar:'images/no-avatar.png') ?>"></a></td>
                 </tr>
-                </tbody>
-            <?php endforeach; ?>
-        </table>
-    </div>
-</div>
-<script type="text/javascript">
-  $(document).ready(function() {
-    $('#list_users').DataTable();
-  } );
-</script>
+            <?php } ?>
+            <?php } ?>
+        </tbody>
+    </table>
